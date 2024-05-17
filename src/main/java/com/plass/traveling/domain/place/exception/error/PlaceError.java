3d8030368aab356aecb6e53116b2755136153884
최미래 <miraechoi@dgsw.hs.kr>
2024-05-18 01:50:48 +0900

    Add :: add PLACE_NO_ACCESS_ERROR

--- src/main/java/com/plass/traveling/domain/place/exception/error/PlaceError.java
+++ src/main/java/com/plass/traveling/domain/place/exception/error/PlaceError.java
@@ -1,4 +1,4 @@
-package com.plass.traveling.global.exception.place;
+package com.plass.traveling.domain.place.exception.error;
 
 import com.plass.traveling.global.exception.error.ErrorProperty;
 import lombok.Getter;
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum PlaceError implements ErrorProperty {
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
-    PLACE_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 장소를 찾을 수 없습니다.");
+    PLACE_NOT_FOUND_EXCEPTION(HttpStatus.NOT_FOUND, "해당 장소를 찾을 수 없습니다."),
+    PLACE_NO_ACCESS_ERROR(HttpStatus.FORBIDDEN, "장소에 대한 권한이 없습니다.");
     private final HttpStatus status;
     private final String message;
 }
