b96a7fae101fd8c7e9fe19ab1fcec31df1fc6578
최미래 <miraechoi@dgsw.hs.kr>
2024-05-18 01:48:17 +0900

    Feat :: create BusinessException

---
+++ src/main/java/com/plass/traveling/global/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.plass.traveling.global.exception;
+
+import com.plass.traveling.global.exception.error.ErrorProperty;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class BusinessException extends RuntimeException{
+    private final ErrorProperty error;
+}
