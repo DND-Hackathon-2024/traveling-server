b51c1b33d6c4513370063be29e24c9e80a80fc04
최미래 <miraechoi@dgsw.hs.kr>
2024-05-18 04:46:23 +0900

    Refactor :: refactor place -> trap

---
+++ src/main/java/com/plass/traveling/domain/trap/presentation/TrapController.java
@@ -0,0 +1,30 @@
+package com.plass.traveling.domain.trap.presentation;
+
+import com.plass.traveling.domain.trap.payload.request.CreateTrapRequest;
+import com.plass.traveling.domain.trap.service.TrapService;
+import com.plass.traveling.global.common.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/trap")
+@RequiredArgsConstructor
+public class TrapController {
+    private final TrapService trapService;
+
+    @GetMapping("/list")
+    public BaseResponse allTraps(){
+        return trapService.findAll();
+    }
+
+    @PostMapping("/create")
+    public BaseResponse createTrap(@RequestBody CreateTrapRequest createTrapRequest) {
+        return trapService.trapCreate(createTrapRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public BaseResponse deleteTrap(@PathVariable Long id){
+        return trapService.trapDelete(id);
+    }
+}
