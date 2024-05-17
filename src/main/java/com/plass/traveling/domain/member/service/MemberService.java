6017cb75d7f0c761be3c7ccf5d8afeb35243dc5c
이슬아 <rheeseula@gmail.com>
2024-05-17 23:52:22 +0900

    Feat :: Member 도메인 Service BaseResponse, CustomException 반영

--- src/main/java/com/plass/traveling/domain/member/service/MemberService.java
+++ src/main/java/com/plass/traveling/domain/member/service/MemberService.java
@@ -1,12 +1,17 @@
 package com.plass.traveling.domain.member.service;
 
+import com.fasterxml.jackson.databind.ser.Serializers;
 import com.plass.traveling.domain.member.dto.req.MemberLoginRequest;
 import com.plass.traveling.domain.member.dto.req.MemberRegisterRequest;
 import com.plass.traveling.domain.member.enums.MemberRoles;
 import com.plass.traveling.domain.member.entity.MemberEntity;
 import com.plass.traveling.domain.member.repository.MemberRepository;
 import com.plass.traveling.global.auth.JwtProvider;
+import com.plass.traveling.global.common.BaseResponse;
+import com.plass.traveling.global.exception.CustomException;
+import com.plass.traveling.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,20 +21,24 @@ public class MemberService {
     private final MemberRepository repository;
     private final JwtProvider provider;
 
-    public String login (MemberLoginRequest dto) {
+    public BaseResponse login (MemberLoginRequest dto) {
         MemberEntity entity = repository.findByPhone(dto.getPhone())
-                .orElseThrow(IllegalAccessError::new);
+                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_EXIST));
 
         if (!entity.getPassword().equals(dto.getPassword())) {
-            throw new IllegalAccessError();
+            throw new CustomException(ErrorCode.MEMBER_NOT_MATCH);
         }
 
-        return provider.generate(entity);
+        return new BaseResponse(
+                HttpStatus.OK,
+                "토큰 발급 성공",
+                provider.generate(entity)
+        );
     }
 
-    public void register (MemberRegisterRequest dto) {
+    public BaseResponse register (MemberRegisterRequest dto) {
         if (repository.existsByPhone(dto.getPhone())) {
-            throw new IllegalAccessError();
+            throw new CustomException(ErrorCode.MEMBER_ALREADY_EXIST);
         }
 
         repository.save(
@@ -38,6 +47,11 @@ public class MemberService {
                         .password(dto.getPassword())
                         .role(MemberRoles.convert(dto.getRole()))
                         .build()
+        );
+
+        return new BaseResponse(
+                HttpStatus.OK,
+                "회원가입 성공"
         );
     }
 
