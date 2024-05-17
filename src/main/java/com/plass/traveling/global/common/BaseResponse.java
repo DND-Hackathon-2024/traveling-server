2b918e751c5d3e5416bf9998dbed8c15a0d97d62
최미래 <miraechoi@dgsw.hs.kr>
2024-05-17 22:30:43 +0900

    Feat :: create baseresponse

---
+++ src/main/java/com/plass/traveling/global/common/BaseResponse.java
@@ -0,0 +1,30 @@
+package com.plass.traveling.global.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+public class BaseResponse {
+
+    private HttpStatus httpStatus;
+    private String massage;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Object data;
+
+    public BaseResponse() {
+        this.httpStatus = HttpStatus.BAD_REQUEST;
+        this.massage = "잘못된 반환입니다.";
+        this.data = null;
+    }
+    public BaseResponse(HttpStatus httpStatus, String massage) {
+        this.httpStatus = httpStatus;
+        this.massage = massage;
+    }
+
+    public BaseResponse(HttpStatus httpStatus, String massage, Object data){
+        this.httpStatus = httpStatus;
+        this.massage = massage;
+        this.data = data;
+    }
+}
