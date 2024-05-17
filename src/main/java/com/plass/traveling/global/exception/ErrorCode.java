7a7927eaa10ec8e62f8a51a76392bb90a8c0b77d
이슬아 <rheeseula@gmail.com>
2024-05-17 23:51:59 +0900

    Feat :: ErrorCode MEMBER_NOT_EXIST, MEMBER_NOT_MATCH 추가

--- src/main/java/com/plass/traveling/global/exception/ErrorCode.java
+++ src/main/java/com/plass/traveling/global/exception/ErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    MEMBER_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "멤버가 이미 존재합니다");
+    MEMBER_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "멤버가 이미 존재합니다"),
+    MEMBER_NOT_EXIST(HttpStatus.BAD_REQUEST, "멤버가 존재하지 않습니다"),
+    MEMBER_NOT_MATCH(HttpStatus.BAD_REQUEST, "멤버 정보가 일치하지 않습니다");
 
     private final HttpStatus httpStatus;
     private final String message;
