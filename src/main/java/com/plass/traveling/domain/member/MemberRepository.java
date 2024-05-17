ba1602e30d3f0ca0cc69abc5c00bc2cdff15ca3c
이슬아 <rheeseula@gmail.com>
2024-05-17 21:54:39 +0900

    Feat :: Member 도메인 Repository 생성

---
+++ src/main/java/com/plass/traveling/domain/member/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.plass.traveling.domain.member;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface MemberRepository extends CrudRepository<MemberEntity, Long> {
+
+}
