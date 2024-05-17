818814c969c3cd8c7256be31cb22b5621901c36d
최미래 <miraechoi@dgsw.hs.kr>
2024-05-18 04:56:24 +0900

    fix error

--- src/main/java/com/plass/traveling/domain/place/service/PlaceService.java
+++
@@ -1,9 +0,0 @@
-package com.plass.traveling.domain.place.service;
-
-import com.plass.traveling.global.common.BaseResponse;
-import org.springframework.stereotype.Service;
-
-@Service
-public interface PlaceService {
-    BaseResponse findAll();
-}
