1d5042b91456822e1c0150a05fb42c5c1dda01ed
최미래 <miraechoi@dgsw.hs.kr>
2024-05-17 21:18:51 +0900

    first commit

---
+++ src/main/java/com/plass/traveling/TravelingServerApplication.java
@@ -0,0 +1,13 @@
+package com.plass.traveling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/plass/traveling/TravelingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plass.traveling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
