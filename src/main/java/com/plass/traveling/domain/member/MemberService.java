a85114ea546bd354979331c6e177d499c492893c
이슬아 <rheeseula@gmail.com>
2024-05-17 23:26:59 +0900

    Feat :: Member 도메인 Service 토큰 발급 & 검증 로직 추가

--- src/main/java/com/plass/traveling/domain/member/MemberService.java
+++ src/main/java/com/plass/traveling/domain/member/MemberService.java
@@ -1,5 +1,6 @@
 package com.plass.traveling.domain.member;
 
+import com.plass.traveling.global.auth.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -7,17 +8,30 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class MemberService {
 
-    private MemberRepository repository;
+    private final MemberRepository repository;
+    private final JwtProvider provider;
 
-    public void login (MemberLoginRequest dto) {
+    public String login (MemberLoginRequest dto) {
+        MemberEntity entity = repository.findByPhone(dto.getPhone())
+                .orElseThrow(IllegalAccessError::new);
 
+        if (!entity.getPassword().equals(dto.getPassword())) {
+            throw new IllegalAccessError();
+        }
+
+        return provider.generate(entity);
     }
 
     public void register (MemberRegisterRequest dto) {
+        if (repository.existsByPhone(dto.getPhone())) {
+            throw new IllegalAccessError();
+        }
+
         repository.save(
                 MemberEntity.builder()
                         .phone(dto.getPhone())
                         .password(dto.getPassword())
+                        .role(MemberRoles.convert(dto.getRole()))
                         .build()
         );
     }
