d87e33c36b6e26de8bf72236f401117fbcf5420e
이슬아 <rheeseula@gmail.com>
2024-05-17 21:54:33 +0900

    Feat :: Member 도메인 Register DTO 생성

---
+++ src/main/java/com/plass/traveling/domain/member/MemberRegisterRequest.java
@@ -0,0 +1,14 @@
+package com.plass.traveling.domain.member;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MemberRegisterRequest {
+
+    private String phone;
+
+    private String password;
+
+}
