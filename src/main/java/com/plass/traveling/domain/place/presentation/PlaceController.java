8d7b5a8cf650d07a426c2f2121be4a767f0933ca
최미래 <miraechoi@dgsw.hs.kr>
2024-05-18 01:53:53 +0900

    Feat :: create Place Controller

--- src/main/java/com/plass/traveling/domain/place/presentation/PlaceController.java
+++ src/main/java/com/plass/traveling/domain/place/presentation/PlaceController.java
@@ -1,22 +1,31 @@
-//package com.plass.traveling.domain.place.presentation;
-//
-//import com.plass.traveling.domain.place.service.PlaceService;
-//import com.plass.traveling.global.common.BaseResponse;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@CrossOrigin(origins = "*", maxAge = 3600)
-//@RestController
-//@RequestMapping("/device")
-//@RequiredArgsConstructor
-//public class PlaceController {
-//    private final PlaceService placeService;
-//
-//    @GetMapping("/list")
-//    public BaseResponse allPlaces(){
-//        return placeService.findAll();
-//    }
-//}
+package com.plass.traveling.domain.place.presentation;
+
+import com.plass.traveling.domain.place.payload.request.CreatePlaceRequest;
+import com.plass.traveling.domain.place.service.PlaceService;
+import com.plass.traveling.global.common.BaseResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/device")
+@RequiredArgsConstructor
+public class PlaceController {
+    private final PlaceService placeService;
+
+    @GetMapping("/list")
+    public BaseResponse allPlaces(){
+        return placeService.findAll();
+    }
+
+    @PostMapping("/create")
+    public BaseResponse createPlace(@RequestBody CreatePlaceRequest createPlaceRequest) {
+        return placeService.placeCreate(createPlaceRequest);
+    }
+
+    @DeleteMapping("/{id}")
+    public BaseResponse deleteVideo(@RequestBody Long id){
+        return placeService.placeDelete(id);
+    }
+
+}
