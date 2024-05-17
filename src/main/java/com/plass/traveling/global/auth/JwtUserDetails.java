f91886096017f9e6921074e763b10f4a14b36dec
이슬아 <rheeseula@gmail.com>
2024-05-17 23:28:12 +0900

    Feat :: JwtUserDetails 구현

---
+++ src/main/java/com/plass/traveling/global/auth/JwtUserDetails.java
@@ -0,0 +1,56 @@
+package com.plass.traveling.global.auth;
+
+import com.plass.traveling.domain.member.MemberEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@AllArgsConstructor
+public class JwtUserDetails implements UserDetails {
+
+    private final MemberEntity member;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+        authorities.add(new SimpleGrantedAuthority(member.getRole().name()));
+
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getPhone();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
