8a085da29d82c49fdef944432eb33012e39c8863
최미래 <miraechoi@dgsw.hs.kr>
2024-05-18 01:49:48 +0900

    Fix :: delete entityToDto

--- src/main/java/com/plass/traveling/domain/place/service/PlaceService.java
+++ src/main/java/com/plass/traveling/domain/place/service/PlaceService.java
@@ -2,29 +2,16 @@ package com.plass.traveling.domain.place.service;
 
 import com.plass.traveling.domain.place.entity.Place;
 import com.plass.traveling.domain.place.payload.request.CreatePlaceRequest;
-import com.plass.traveling.domain.place.payload.request.PlaceDto;
 import com.plass.traveling.global.common.BaseResponse;
 
 public interface PlaceService {
     BaseResponse findAll();
 
-    default PlaceDto entityToDto(Place entity){
-        return PlaceDto.builder()
-                .id(entity.getId())
-                .placeName((entity.getPlaceName()))
-                .placeDesc(entity.getPlaceDesc())
-                .address(entity.getAddress())
-                .couponId(entity.getCouponId())
-                .imgUrl(entity.getImgUrl())
-                .build();
-    }
+    BaseResponse placeCreate(CreatePlaceRequest placeCreateRequest);
 
-    default Place dtoToEntity(CreatePlaceRequest createPlaceRequest,
-                              String placeName,
-                              String placeDecs,
-                              String address,
-                              String couponId,
-                              Long imgUrl){
+    BaseResponse placeDelete(Long placeId);
+
+    default Place dtoToEntity(CreatePlaceRequest createPlaceRequest){
         return Place.builder()
                 .placeName(createPlaceRequest.placeName())
                 .placeDesc(createPlaceRequest.placeDesc())
