55e42c173258a13e9e034a94c02061474b78af51
yeseong0412 <yeseong0412@dgsw.hs.kr>
2024-05-17 22:19:15 +0900

    Feat :: 유저 아이디 받아오는 어노테이션 생성

---
+++ src/main/java/com/plass/traveling/global/common/annotation/GetAuthenticatedId.java
@@ -0,0 +1,14 @@
+package com.plass.traveling.global.common.annotation;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.PARAMETER)
+@AuthenticationPrincipal(expression = "#this == 'anonymousUser' ? null : #this.id")
+public @interface GetAuthenticatedId {
+}
